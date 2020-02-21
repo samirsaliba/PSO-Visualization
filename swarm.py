cbae2b0b28e37e2b2754f1480a813e5949ce4836
Samir Saliba Jr <samirtsj@gmail.com>
2020-02-20 22:37:39 -0300

    Add files via upload

--- swarm.py
+++ swarm.py
@@ -1,7 +1,20 @@
-# Particle Swarm Optimization Simple Visualization
-# Click on screen to set 'treasure' location, which particles will attempt to 'find'
-# Had some help from: 
+# PSO-Visualization
+# Simple visualization of how the Particle Swarm Optimization algorithm works using pygame.
+# Click on screen to set 'treasure' location, which particles will attempt to 'find'.
+# Had some help from the following articles: 
 # https://medium.com/analytics-vidhya/implementing-particle-swarm-optimization-pso-algorithm-in-python-9efc2eb179a6
+# And
+# https://towardsdatascience.com/particle-swarm-optimisation-in-machine-learning-b01b1d2ad8a8
+# Thanks @IranNeto, @tstreamDOTh
+
+#---
+# Visualização simples de como funciona o algoritmo de otimização por enxame de partículas usando pygame.
+# Clique na tela para setar a posição do 'tesouro', o qual as partículas vão tentar 'procurar'.
+# Obtive alguma ajuda através dos seguintes artigos:
+# https://medium.com/analytics-vidhya/implementing-particle-swarm-optimization-pso-algorithm-in-python-9efc2eb179a6
+# e
+# https://towardsdatascience.com/particle-swarm-optimisation-in-machine-learning-b01b1d2ad8a8
+# Obrigado @IranNeto, @tstreamDOTh
 
 import pygame
 import random as rd
@@ -43,7 +56,6 @@ class Particle():
         self.fitness = math.sqrt((x-treasurex)**2 + (y-treasurey)**2)
         self.set_pbest()
         self.set_velocity(gbest_position, gbest_value)
-        print(self.velocity)
 
     def set_pbest(self):
         if(self.fitness < self.pbest_value):
